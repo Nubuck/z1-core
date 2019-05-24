c1cbc05c6845d1012434a20ea796573136008fc6
Barry Buck <jozi011@gmail.com>
2019-05-24 15:15:30 +0200

    db flavors wip

---
+++ libs/api-box-nedb/src/main.js
@@ -0,0 +1,6 @@
+import { task } from '@z1/preset-task'
+import { Nedb, FeathersNedb } from '@z1/preset-feathers-server-nedb'
+
+// main
+const createModel = task(t => (name, props, options = {}) => () => {})
+const createDBConnection = task(t => app => {})

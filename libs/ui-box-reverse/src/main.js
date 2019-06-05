8ef7fdeee9cb57f539e15a58b622a6dfb2462346
Barry Buck <jozi011@gmail.com>
2019-06-05 14:28:07 +0200

    proof cleanup

--- libs/ui-box-reverse/src/main.js
+++ libs/ui-box-reverse/src/main.js
@@ -22,49 +22,50 @@ export const cssToBox = task(t => (css = '') => {
   if (t.isZeroLen(css)) {
     return {}
   }
-  const output = t.map(className => {
-    const prefixList = t.split(':', className)
-    const base = t.gt(t.length(prefixList), 1)
-      ? {
-          css: t.head(t.tail(prefixList)),
-          prefix: t.head(prefixList),
-        }
-      : {
-          css: t.head(prefixList),
-          prefix: null,
-        }
-    const defChunks = t.split('-', base.css)
-    const keyChunk = t.head(defChunks)
-    const matchChunk = t.gt(t.length(defChunks), 1)
-      ? t.head(t.tail(defChunks))
-      : null
-    const def = defs[keyChunk]
-    const nextDef = t.isType(def.map, 'Array')
-      ? t.find(
-          item => t.or(t.eq(item.key, keyChunk), t.eq(item.key, matchChunk)),
-          def.map
-        )
-      : def
-    return t.mergeAll([
-      base,
-      {
-        key: keyChunk,
-        match: matchChunk,
-        chunks: defChunks,
-      },
-      nextDef,
-    ])
-  }, t.split(' ', css))
-
-  const groupOutput = t.groupBy(prop => prop.map, output)
-  const nextBox = t.mapObjIndexed((val, key) => {
-    const boxProp = boxProps[key]
-    const nextProp = boxProp(splitProps(val))
-    return nextProp
-  }, groupOutput)
-  console.log('NEXT BOX:', nextBox)
-
-  return output
+  return t.mapObjIndexed(
+    (val, key) => {
+      const boxProp = boxProps[key]
+      const nextProp = boxProp(splitProps(val))
+      return nextProp
+    },
+    t.groupBy(
+      prop => prop.map,
+      t.map(className => {
+        const prefixList = t.split(':', className)
+        const base = t.gt(t.length(prefixList), 1)
+          ? {
+              css: t.head(t.tail(prefixList)),
+              prefix: t.head(prefixList),
+            }
+          : {
+              css: t.head(prefixList),
+              prefix: null,
+            }
+        const defChunks = t.split('-', base.css)
+        const keyChunk = t.head(defChunks)
+        const matchChunk = t.gt(t.length(defChunks), 1)
+          ? t.head(t.tail(defChunks))
+          : null
+        const def = defs[keyChunk]
+        const nextDef = t.isType(def.map, 'Array')
+          ? t.find(
+              item =>
+                t.or(t.eq(item.key, keyChunk), t.eq(item.key, matchChunk)),
+              def.map
+            )
+          : def
+        return t.mergeAll([
+          base,
+          {
+            key: keyChunk,
+            match: matchChunk,
+            chunks: defChunks,
+          },
+          nextDef,
+        ])
+      }, t.split(' ', css))
+    )
+  )
 })
 
 // test

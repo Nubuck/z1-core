45f8548d75800b82f5fa651ed222bff8a52962ef
Barry Buck <Nubuck@users.noreply.github.com>
2019-07-13 04:57:46 +0200

    Update Primatives.js

--- libs/ui-box-elements/src/Primatives.js
+++ libs/ui-box-elements/src/Primatives.js
@@ -8,19 +8,19 @@ export const Box = task(t => props => {
   const box = t.pathOr(null, ['box'], props)
   const next = t.pathOr(null, ['next'], props)
   const stretch = t.pathOr(null, ['stretch'], props)
-  const stretchProps = t.isNil(stretch)
-    ? {}
-    : {
-        alignSelf: 'stretch',
-        flex: 'auto',
-      }
+  
   return React.createElement(
     Element,
     t.merge(t.omit(['as', 'box', 'className', 'stretch', 'next'], props), {
       className: t.and(t.and(t.isNil(box), t.isNil(stretch)), t.isNil(next))
         ? t.pathOr('', ['className'], props)
         : uiBox(box || {})
-            .next(stretchProps)
+            .next(t.isNil(stretch)
+              ? {}
+              : {
+                  alignSelf: 'stretch',
+                  flex: 'auto',
+                })
             .next({
               className: t.pathOr(box.className || '', ['className'], props),
             })

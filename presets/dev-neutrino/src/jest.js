3f8b0a9ab4387b06584045a5027259a4b812e518
Barry Buck <jozi011@gmail.com>
2020-01-02 12:19:30 +0200

    neutrino migration wip

--- presets/dev-neutrino/src/jest.js
+++ presets/dev-neutrino/src/jest.js
@@ -1,8 +1,1 @@
 module.exports = require('@neutrinojs/jest')
-// const jest = require('@neutrinojs/jest');
-
-// module.exports = {
-//   use: [
-//     jest()
-//   ]
-// }
--- presets/dev-neutrino/src/uglify.js
+++
@@ -1,1 +0,0 @@
-// module.exports = require('neutrino-middleware-uglifyjs')

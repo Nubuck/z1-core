b5e56d7a54947bc44497ea70c30ebc54695e963d
Barry Buck <jozi011@gmail.com>
2020-01-14 00:14:41 +0200

    macro form dev

--- libs/feature-macros/src/index.js
+++ libs/feature-macros/src/index.js
@@ -1,5 +1,6 @@
+import { fn } from '@z1/lib-feature-box'
 import { routeView as rv } from './main'
 
 // outs
 export const routeView = rv
-export default { routeView: rv }
+export default { routeView: rv, fn }
--- libs/feature-macros/src/main.js
+++ libs/feature-macros/src/main.js
@@ -2,9 +2,7 @@
 import { create } from './create'
 import { combine } from './combine'
 import { configure } from './configure'
-// import { query } from './query'
 import { render } from './render'
-// import { routing } from './routing'
 import { types } from './types'
 
 // main
@@ -12,8 +10,6 @@ export const routeView = {
   create,
   combine,
   configure,
-  // query,
   render,
-  // routing,
   types,
 }

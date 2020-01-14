19de47c05f9af128a6ea1d64c716ec46b5ce0dac
Barry Buck <jozi011@gmail.com>
2020-01-14 22:22:24 +0200

    agent example deps work

--- examples/agent/src/main/index.js
+++ examples/agent/src/main/index.js
@@ -1,5 +1,8 @@
 import { app } from 'electron'
+import os from 'os'
 import robot from 'robot-js'
+import screenshot from 'screenshot-desktop'
+import sysInfo from 'systeminformation'
 
 process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at: Promise ', p, reason)
@@ -18,8 +21,19 @@ process.on('exit', code => {
 // app events
 app.on('ready', () => {
   console.log('APP READY')
+  console.log('OS HOST', os.hostname())
   if (robot.Window.isAxEnabled()) {
     const active = robot.Window.getActive()
-    console.log('ACTIVE WINDOW', active.getTitle())
+    console.log('ACTIVE WINDOW ', active.getTitle())
+    screenshot
+      .listDisplays()
+      .then(displays => console.log('DISPLAYS ', displays))
+      .catch(err => console.log('DISPLAYS ERR ', err))
+    sysInfo
+      .system()
+      .then(sys => {
+        console.log('SYSTEM INFO HARDWARE UUID ', sys.uuid)
+      })
+      .catch(err => console.log('SYSTEM INFO ERR ', err))
   }
 })

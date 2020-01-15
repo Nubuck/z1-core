773e9b1a66a6a713c59e0333c9ebb764187ef4a8
Barry Buck <jozi011@gmail.com>
2020-01-15 12:36:59 +0200

    machine account dev

--- kits/machine-account-server-core/src/strategy.js
+++ kits/machine-account-server-core/src/strategy.js
@@ -1,10 +1,12 @@
-export const strategy => z => {
-  return class MachineStategy extends z.FeathersAuth.AuthenticationBaseStrategy {
-    async authenticate(authentication, params) {
-      return {
-        machine: null,
-        user: null,
+export const strategy = z =>
+  z.featureBox.fn(
+    (t, a) =>
+      class MachineStategy extends z.FeathersAuth.AuthenticationBaseStrategy {
+        async authenticate(authentication, params) {
+          return {
+            machine: null,
+            user: null,
+          }
+        }
       }
-    }
-  }
-} 
+  )

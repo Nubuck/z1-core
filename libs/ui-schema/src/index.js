70210f598681087c5cefe8dbf0bba117560cb39d
Barry Buck <jozi011@gmail.com>
2020-01-20 00:12:09 +0200

    nav schema next dev

--- libs/ui-schema/src/index.js
+++ libs/ui-schema/src/index.js
@@ -5,12 +5,21 @@ const renderChildren = fn(t => (ctx, parent, children) => {
   return t.reduce((cx, ff) => ff(cx, parent), ctx, children || [])
 })
 const item = fn(t => (path = '/', props = {}, children = []) => {
-  const pathKey = t.eq(path, '/') ? '_' : t.to.snakeCase(path)
   const hasChildren = t.notZeroLen(children)
   return (ctx, parent) => {
+    const nextPath = t.startsWith('~', path)
+      ? `/${t.replace(/_/g, '/', parent)}/${t.replace(
+          '/',
+          '',
+          t.replace('~', '', path)
+        )}`
+      : t.startsWith('/', path)
+      ? path
+      : `/${path}`
+    const pathKey = t.eq(nextPath, '/') ? '_' : t.to.snakeCase(nextPath)
     return t.merge(ctx, {
       [pathKey]: {
-        path,
+        path: nextPath,
         parent,
         props,
         hasChildren,
@@ -20,40 +29,54 @@ const item = fn(t => (path = '/', props = {}, children = []) => {
   }
 })
 const nav = fn(t => factory => {
-  const navSchema = factory(item)
-  return navSchema({}, 'root')
+  const nextSchema = factory(item)
+  return t.isType(nextSchema, 'array')
+    ? renderChildren({}, 'root', nextSchema)
+    : nextSchema({}, 'root')
 })
 
 // print example
-const demo = nav(n =>
+const demo = nav(n => [
   n('/', { title: 'home' }, [
     n('/about', { title: 'about' }),
     n('/contact', { title: 'contact' }),
     n('/products', { title: 'products' }, [
-      n('/products/type1', { title: 'type 1' }, [
-        n('/products/type1/about', { title: 'type 1 about' }),
-        n('/products/type1/pricing', { title: 'type 1 pricing' }),
+      n('~/type1', { title: 'type 1' }, [
+        n('~/about', { title: 'type 1 about' }),
+        n('~/pricing', { title: 'type 1 pricing' }),
       ]),
-      n('/products/type2', { title: 'type 2' }, [
-        n('/products/type2/about', { title: 'type 2 about' }),
-        n('/products/type2/pricing', { title: 'type 2 pricing' }),
-        n('/products/type2/reviews', { title: 'type 2 reviews' }),
+      n('~/type2', { title: 'type 2' }, [
+        n('~/about', { title: 'type 2 about' }),
+        n('~/pricing', { title: 'type 2 pricing' }),
+        n('~/reviews', { title: 'type 2 reviews' }),
       ]),
     ]),
-    n('/specials', { title: 'contact' }, [
-      n('/specials/latest', { title: 'contact' }),
-    ]),
-  ])
-)
+    n('/specials', { title: 'contact' }, [n('~/latest', { title: 'contact' })]),
+  ]),
+  n('/partners', { title: 'partners' }, [
+    n('~/about', { title: 'partners about' }),
+    n('~/contact', { title: 'partners contact' }),
+  ]),
+])
 const print = fn(t => () => {
-  console.log('NAV SCHEMA', demo._)
+  console.log('NAV SCHEMA', demo)
 
   t.forEach(([key, child]) => {
-    console.log('NAV SCHEMA Child', key, child)
+    console.log('SCHEMA Child', key, child)
     if (child.hasChildren) {
-      console.log('NAV SCHEMA Child children', key, child.children)
+      t.forEach(([childKey, grandChild]) => {
+        console.log('SCHEMA Child Grandchild', key, childKey, grandChild)
+        if (grandChild.hasChildren) {
+          console.log(
+            'SCHEMA Child Grandchild children',
+            key,
+            childKey,
+            grandChild.children
+          )
+        }
+      }, t.to.pairs(child.children))
     }
-  }, t.to.pairs(demo._.children))
+  }, t.to.pairs(demo))
 })
 
 print()

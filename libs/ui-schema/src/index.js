a437ba4973f59986aec71bba8bf8bd63e6644d35
Barry Buck <jozi011@gmail.com>
2020-01-19 18:47:28 +0200

    ui schema dev

--- libs/ui-schema/src/index.js
+++ libs/ui-schema/src/index.js
@@ -48,7 +48,7 @@ const keys = {
     help: 'help',
     number: 'number',
   },
-  prop: {
+  field: {
     title: 'title',
     desc: 'description',
     type: 'type',
@@ -66,80 +66,122 @@ const keys = {
   },
 }
 
-const field = fn(t => (nameOrProps, propsOrChildren, otherChildren) => {
-  const name = t.isType(nameOrProps, 'string') ? nameOrProps : 'root'
-  const props = t.eq(name, 'root') ? nameOrProps : propsOrChildren
-  const children = t.eq(name, 'root') ? propsOrChildren : otherChildren
-  const fieldKey = t.to.camelCase(name)
-  const nextProps = t.omit(['ui', 'required'], props)
-  const nextRequired = t.pathOr(false, ['required'], props)
-  if (t.neq(name, 'root')) {
-    if (t.includes(nextProps.type, ['object', 'array'])) {
-      const childKey = t.match({
-        object: 'properties',
-        array: 'items',
-      })(nextProps.type)
-      return ctx => {
-        const next = t.reduce(
-          (cx, ff) => ff(cx),
-          { props: {}, ui: t.pathOr({}, ['ui'], props), required: [] },
-          children || []
-        )
-        const itemKey = t.eq(childKey, 'items')
-          ? t.head(t.keys(next.props))
-          : ''
-        return {
-          props: t.merge(t.pathOr({}, ['props'], ctx), {
-            [fieldKey]: t.merge(nextProps, {
-              required: next.required,
-              [childKey]: t.eq(childKey, 'items')
-                ? t.pathOr({}, [itemKey], next.props)
-                : next.props,
-            }),
-          }),
-          ui: t.merge(t.pathOr({}, ['ui'], ctx), {
-            [fieldKey]: t.eq(childKey, 'items')
-              ? { items: t.pathOr({}, [itemKey], next.ui) }
-              : next.ui,
-          }),
-          required: t.eq(nextRequired, true)
-            ? t.concat(t.pathOr([], ['required'], ctx), [fieldKey])
-            : t.pathOr([], ['required'], ctx),
-        }
-      }
+const none = 'none'
+
+const field = fn(
+  t => (
+    nameOrProps,
+    propsOrChildren,
+    childrenOrAdditional,
+    otherAdditional
+  ) => {
+    const name = t.isType(nameOrProps, 'string') ? nameOrProps : none
+    const props = t.eq(name, none) ? nameOrProps : propsOrChildren
+    const children = t.eq(name, none) ? propsOrChildren : childrenOrAdditional
+    const additional = t.eq(name, none) ? childrenOrAdditional : otherAdditional
+    const ui = t.pathOr({}, ['ui'], props)
+    const required = t.pathOr(false, ['required'], props)
+    // next
+    const fieldKey = t.to.camelCase(name)
+    const fieldType = t.pathOr('string', ['type'], props)
+    const fieldProps = t.omit(['ui', 'required'], props)
+    // yield
+    return (ctx, parent) => {
+      const renderField = t.match({
+        object: () => {
+          return {}
+        },
+        array: () => {
+          return {}
+        },
+        _: () => {
+          return {}
+        },
+      })(fieldType)
+
+      const nextSchema = t.match({
+        root: () => {
+          return {}
+        },
+        object: () => {
+          return {}
+        },
+        array: () => {
+          return {}
+        },
+      })(parent)
+
+      return t.isType(nextSchema, 'function') ? nextSchema() : ctx
     }
-    return ctx => {
-      return {
-        props: t.merge(t.pathOr({}, ['props'], ctx), {
-          [fieldKey]: nextProps,
-        }),
-        ui: t.merge(t.pathOr({}, ['ui'], ctx), {
-          [fieldKey]: t.pathOr({}, ['ui'], props),
-        }),
-        required: t.eq(nextRequired, true)
-          ? t.concat(t.pathOr([], ['required'], ctx), [fieldKey])
-          : t.pathOr([], ['required'], ctx),
-      }
-    }
+
+    // if (t.neq(name, none)) {
+    //   if (t.includes(nextProps.type, ['object', 'array'])) {
+    //     const childKey = t.match({
+    //       object: 'properties',
+    //       array: 'items',
+    //     })(nextProps.type)
+    //     return ctx => {
+    //       const next = t.reduce(
+    //         (cx, ff) => ff(cx),
+    //         { props: {}, ui: t.pathOr({}, ['ui'], props), required: [] },
+    //         children || []
+    //       )
+    //       const itemKey = t.eq(childKey, 'items')
+    //         ? t.head(t.keys(next.props))
+    //         : ''
+    //       return {
+    //         props: t.merge(t.pathOr({}, ['props'], ctx), {
+    //           [fieldKey]: t.merge(nextProps, {
+    //             required: next.required,
+    //             [childKey]: t.eq(childKey, 'items')
+    //               ? t.pathOr({}, [itemKey], next.props)
+    //               : next.props,
+    //           }),
+    //         }),
+    //         ui: t.merge(t.pathOr({}, ['ui'], ctx), {
+    //           [fieldKey]: t.eq(childKey, 'items')
+    //             ? { items: t.pathOr({}, [itemKey], next.ui) }
+    //             : next.ui,
+    //         }),
+    //         required: t.eq(nextRequired, true)
+    //           ? t.concat(t.pathOr([], ['required'], ctx), [fieldKey])
+    //           : t.pathOr([], ['required'], ctx),
+    //       }
+    //     }
+    //   }
+    //   return ctx => {
+    //     return {
+    //       props: t.merge(t.pathOr({}, ['props'], ctx), {
+    //         [fieldKey]: nextProps,
+    //       }),
+    //       ui: t.merge(t.pathOr({}, ['ui'], ctx), {
+    //         [fieldKey]: t.pathOr({}, ['ui'], props),
+    //       }),
+    //       required: t.eq(nextRequired, true)
+    //         ? t.concat(t.pathOr([], ['required'], ctx), [fieldKey])
+    //         : t.pathOr([], ['required'], ctx),
+    //     }
+    //   }
+    // }
+    // const next = t.reduce(
+    //   (ctx, ff) => ff(ctx),
+    //   { props: {}, ui: t.pathOr({}, ['ui'], props), required: [] },
+    //   children || []
+    // )
+    // return {
+    //   schema: t.merge(nextProps, {
+    //     required: next.required,
+    //     [keys.prop.props]: next.props,
+    //   }),
+    //   uiSchema: next.ui,
+    // }
   }
-  const next = t.reduce(
-    (ctx, ff) => ff(ctx),
-    { props: {}, ui: t.pathOr({}, ['ui'], props), required: [] },
-    children || []
-  )
-  return {
-    schema: t.merge(nextProps, {
-      required: next.required,
-      [keys.prop.props]: next.props,
-    }),
-    uiSchema: next.ui,
-  }
-})
+)
 
 const form = fn(t => factory => {
   const formSchema = factory(field, keys)
   // TODO:
-  // const result = formSchema({ schema: {}, uiSchema: {}, required: [] }, true)
+  // const result = formSchema({ schema: {}, uiSchema: {}, required: [] }, 'root')
   return formSchema
 })
 

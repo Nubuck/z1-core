12de17c14ec776640d24d2159d40c7827e3bdd95
nubuck <jozi011@gmail.com>
2020-01-25 16:49:04 +0200

    preset notNil helps

--- examples/web/src/parts/ui/ListItem.jsx
+++ examples/web/src/parts/ui/ListItem.jsx
@@ -8,46 +8,51 @@ import {
   Box,
   Button,
   Avatar,
+  MapIndexed,
 } from '@z1/lib-ui-box-elements'
 
 // main
 const renderListItem = z.fn(t => props => {
+  // mode:
+  const selectable = t.pathOr(false, ['selectable'], props)
   // status:
-  // loading
-  // disabled
-  // selected
-
-  // actions: to, onClick, onSelect
-
-  // cols: select, avatar, title, content, last
-
+  const loading = t.pathOr(false, ['loading'], props)
+  const disabled = t.pathOr(false, ['disabled'], props)
+  const selected = t.pathOr(false, ['selected'], props)
+  // actions:
+  const to = t.pathOr(null, ['to'], props)
+  const onClick = t.pathOr(null, ['onClick'], props)
+  const onSelect = t.pathOr(null, ['onSelect'], props)
+  // layout:
+  const cols = t.pathOr({}, ['cols'], props)
+  const colSelect = t.pathOr(null, ['select'], cols)
+  const colAvatar = t.pathOr(null, ['avatar'], cols)
+  const colTitle = t.pathOr(null, ['title'], cols)
+  const colContent = t.pathOr(null, ['content'], cols)
+  const colLast = t.pathOr(null, ['last'], cols)
+  const hasCols = t.anyOf([colSelect, colAvatar, colTitle, colContent, colLast])
   // select col:
-  // select
-  // -> icon: on / of
-
+  const select = t.pathOr(null, ['select'], props)
   // avatar col:
-  // avatar
-  // caption
-  // -> text
-
+  const avatar = t.pathOr(null, ['avatar'], props)
+  const caption = t.pathOr(null, ['caption'], props)
   // title col:
-  // title
-  // -> text
-  // subtitle
-  // -> icon, label
-
+  const title = t.pathOr(null, ['title'], props)
+  const subtitle = t.pathOr(null, ['subtitle'], props)
   // content col:
-  // children
-
+  const content = t.pathOr(null, ['content'], props)
   // last col:
-  // stamp
-  // -> icon, label
-  // buttons[]
-  // -> button
-
+  const stamp = t.pathOr(null, ['stamp'], props)
+  const buttons = t.pathOr(null, ['buttons'], props)
+  // nested
+  const nested = t.pathOr(null, ['nested'], props)
+  const children = t.pathOr(null, ['children'], props)
   // colors: on / off / selected
+  const colors = t.pathOr(null, ['colors'], props)
+  const color = t.pathOr(null, ['color'], props)
   const nextProps = t.omit(
     [
+      'selectable',
       'loading',
       'disabled',
       'selected',
@@ -55,22 +60,57 @@ const renderListItem = z.fn(t => props => {
       'onClick',
       'onSelect',
       'cols',
-      'avatar',
       'select',
+      'avatar',
       'caption',
       'title',
       'subtitle',
-      'children',
       'content',
       'stamp',
       'buttons',
+      'nested',
+      'children',
       'colors',
       'color',
-      'nested',
     ],
     props
   )
-  return null
+  return (
+    <Col {...nextProps}>
+      <Row key="row-main">
+        <Col key="col-select">
+          <Button key="selector" />
+        </Col>
+        <Col key="col-avatar">
+          <Row x="center" y="center" key="row-avatar">
+            <Avatar key="avatar" />
+          </Row>
+          <Row key="caption" />
+        </Col>
+        <Col key="col-title">
+          <Row key="title" />
+          <Row key="subtitle" />
+        </Col>
+        <Col key="col-content">
+          <Row key="content" />
+        </Col>
+        <Col key="col-last">
+          <Row key="stamp" />
+          <Row key="buttons">
+            <MapIndexed
+              items={[]}
+              render={(button, index) => (
+                <Button key={`li-btn-${index}`} {...buttonx} />
+              )}
+            />
+          </Row>
+        </Col>
+      </Row>
+      <Row key="row-nested">
+        <Col key="col-nested"></Col>
+      </Row>
+    </Col>
+  )
 })
 
 export class ListItem extends React.Component {

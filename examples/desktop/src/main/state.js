b209fbeaa9092ea61759906f0f142b793a26ccd2
Barry Buck <jozi011@gmail.com>
2020-01-27 23:36:05 +0200

    machine account node dev

--- examples/desktop/src/main/state.js
+++ examples/desktop/src/main/state.js
@@ -1,11 +1,21 @@
 import z from '@z1/lib-state-box-node'
 
+// types
+const authStatus = {
+  init: 'init',
+  waiting: 'auth-waiting',
+  loading: 'auth-loading',
+  success: 'auth-success',
+  fail: 'auth-fail',
+}
+
 // main
 const appState = z.fn((t, a) =>
   z.create('app', [
     {
       initial: {
-        status: 'anon',
+        connected: false,
+        status: authStatus.init,
         error: null,
         agent: null,
         auth: null,
@@ -13,111 +23,173 @@ const appState = z.fn((t, a) =>
       },
       mutations(m) {
         return [
-          m(['boot', 'bootComplete'], (state, action) => {
+          m(['boot'], (state, action) => {
             return t.merge(state, action.payload)
           }),
-          m(['authComplete'], (state, action) => {
+          m(['connection'], (state, action) => {
+            return t.merge(state, { connected: action.payload || false })
+          }),
+          m('authenticate', state => {
+            return t.merge(state, {
+              status: authStatus.waiting,
+              error: null,
+            })
+          }),
+          m('authenticateComplete', (state, action) => {
             return t.merge(state, action.payload)
           }),
         ]
       },
-      effects(fx, { actions, mutators }) {
+      effects(fx, box) {
         return [
-          fx([actions.boot], async (ctx, dispatch, done) => {
-            const [serviceErr, services] = await a.of(ctx.sysInfo.services('*'))
-            if (serviceErr) {
-              console.log('ERR finding services', serviceErr)
-            }
-            const matcher = t.globrex('**brave**')
-            const nextServices = t.filter(
-              service => matcher.regex.test(service.name),
-              services || []
-            )
-            const [agentErr, agent] = await a.of(
-              ctx.machine.account({ role: 'agent' })
-            )
-            if (agentErr) {
-              dispatch(
-                mutators.bootComplete({
-                  agent: null,
-                  error: agentErr,
-                  services: nextServices,
-                })
-              )
-            } else {
-              const [authErr, authResult] = await a.of(
-                ctx.api.authenticate({
-                  strategy: 'machine',
-                  hashId: agent.user.hashId,
-                })
-              )
-              dispatch(
-                mutators.bootComplete({
-                  agent,
-                  error: authErr,
-                  auth: authResult,
-                  services: nextServices,
-                })
-              )
-              done()
-            }
-          }),
-          fx([actions.bootComplete], async (ctx, dispatch, done) => {
-            const state = ctx.getState()
-            const accessToken = t.pathOr(
-              null,
-              ['app', 'auth', 'accessToken'],
-              state
-            )
-            if (t.notNil(accessToken)) {
-              dispatch(mutators.authComplete({ status: 'authenticated' }))
-              done()
-            } else {
-              const agent = t.pathOr({}, ['app', 'agent'], state)
-              const [systemErr, systemResult] = await a.of(
-                ctx.machine.system(agent.machine)
-              )
-              const [accountErr, account] = await a.of(
-                ctx.api
-                  .service('machine-account')
-                  .create(
-                    t.isNil(systemErr)
-                      ? { machine: systemResult, user: agent.user }
-                      : agent
+          fx(
+            [box.actions.boot, box.actions.connection],
+            (ctx, dispatch, done) => {
+              const account = t.path(['app'], ctx.getState())
+              if (
+                t.or(
+                  t.not(account.connected),
+                  t.and(
+                    t.eq(account.status, authStatus.success),
+                    t.eq(account.connected, true)
                   )
-              )
-              if (accountErr) {
+                )
+              ) {
+                done()
+              } else {
+                dispatch(box.mutators.authenticate())
+                done()
+              }
+            }
+          ),
+          fx([box.actions.authenticate], async (ctx, dispatch, done) => {
+            try {
+              if (
+                t.not(t.pathOr(false, ['app', 'connected'], ctx.getState()))
+              ) {
+                console.log('auth not connected')
                 dispatch(
-                  mutators.authComplete({
-                    error: accountErr,
+                  box.mutators.authenticateComplete({
+                    status: authStatus.fail,
+                    agent: null,
+                    error: new Error('Not connected'),
                   })
                 )
+              } else {
+                const [reAuthErr, reAuthResult] = await a.of(
+                  ctx.api.authentication.reAuthenticate()
+                )
+                if (reAuthErr) {
+                  console.log('reauth failed')
+                  const [agentErr, agent] = await a.of(
+                    ctx.machine.account({ role: 'agent' })
+                  )
+                  if (agentErr) {
+                    console.log('agent failed')
+                    dispatch(
+                      box.mutators.authenticateComplete({
+                        status: authStatus.fail,
+                        agent: null,
+                        error: agentErr,
+                      })
+                    )
+                    done()
+                  } else {
+                    const [authErr, authResult] = await a.of(
+                      ctx.api.authenticate({
+                        strategy: 'machine',
+                        hashId: agent.user.hashId,
+                      })
+                    )
+                    if (authErr) {
+                      console.log('auth failed')
+                      const [systemErr, systemResult] = await a.of(
+                        ctx.machine.system(agent.machine)
+                      )
+                      const [accountErr, account] = await a.of(
+                        ctx.api
+                          .service('machine-account')
+                          .create(
+                            t.isNil(systemErr)
+                              ? { machine: systemResult, user: agent.user }
+                              : agent
+                          )
+                      )
+                      if (accountErr) {
+                        console.log('account failed')
+                        dispatch(
+                          box.mutators.authenticateComplete({
+                            status: authStatus.fail,
+                            agent: null,
+                            error: accountErr,
+                          })
+                        )
+                        done()
+                      } else {
+                        const [nextAuthErr, nextAuthResult] = await a.of(
+                          ctx.api.authenticate({
+                            strategy: 'machine',
+                            hashId: account.user.hashId,
+                          })
+                        )
+                        if (nextAuthErr) {
+                          console.log('next auth failed')
+                          dispatch(
+                            box.mutators.authenticateComplete({
+                              status: authStatus.fail,
+                              agent: null,
+                              error: nextAuthErr,
+                            })
+                          )
+                          done()
+                        } else {
+                          dispatch(
+                            box.mutators.authenticateComplete({
+                              status: authStatus.success,
+                              agent: t.pick(
+                                ['machine', 'user'],
+                                nextAuthResult
+                              ),
+                              error: null,
+                            })
+                          )
+                          done()
+                        }
+                      }
+                    } else {
+                      dispatch(
+                        box.mutators.authenticateComplete({
+                          status: authStatus.success,
+                          agent: t.pick(['machine', 'user'], authResult),
+                          error: null,
+                        })
+                      )
+                    }
+                  }
+                } else {
+                  dispatch(
+                    box.mutators.authenticateComplete({
+                      status: authStatus.success,
+                      agent: t.pick(['machine', 'user'], reAuthResult),
+                      error: null,
+                    })
+                  )
+                }
               }
-              const [authErr, authResult] = await a.of(
-                ctx.api.authenticate({
-                  strategy: 'machine',
-                  hashId: account.user.hashId,
-                })
-              )
-              dispatch(
-                mutators.authComplete({
-                  agent: account,
-                  error: authErr,
-                  auth: authResult,
-                })
-              )
+            } catch (e) {
+              console.log('AUTH ERR', e)
               done()
             }
           }),
         ]
       },
-      onInit({ dispatch, mutators, api }) {
-        dispatch(mutators.boot({}))
-        api.io.on('connect', () => {
-          console.log('API CONNECT')
+      onInit(ctx) {
+        ctx.dispatch(ctx.mutators.boot())
+        ctx.api.io.on('connect', () => {
+          ctx.dispatch(ctx.mutators.connection(true))
         })
-        api.io.on('disconnect', () => {
-          console.log('API DISCONNECT')
+        ctx.api.io.on('disconnect', () => {
+          ctx.dispatch(ctx.mutators.connection(false))
         })
       },
     },

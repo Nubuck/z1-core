5b28ac1d9ccd2db88ee29d631079c15d623da48b
Barry Buck <jozi011@gmail.com>
2020-03-02 10:24:35 +0200

    added async trampoline to preset-task

--- presets/task/src/async.js
+++ presets/task/src/async.js
@@ -11,9 +11,18 @@ import {
   success,
   result,
 } from './awaiting'
+import { TASK } from './sync'
 
 const of = function to(promise) {
   return promise.then(result => [null, result]).catch(err => [err, null])
+}
+
+const trampoline = fn => async (...args) => {
+  let result = await fn(...args)
+  while (TASK.isType(result, 'function')) {
+    result = await result()
+  }
+  return result
 }
 
 export const TASK_ASYNC = {
@@ -29,4 +38,5 @@ export const TASK_ASYNC = {
   success,
   result,
   of,
+  trampoline,
 }

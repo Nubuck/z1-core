24430d01a59ff66c2f27e5062982a98a8bfe4e07
Barry Buck <jozi011@gmail.com>
2020-04-18 18:11:51 +0200

    added elseRender to When and emptyRender to MapIndexed ui box elements

--- libs/ui-box-elements/src/MapIndexed.jsx
+++ libs/ui-box-elements/src/MapIndexed.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { fn } from '@z1/lib-ui-box'
 
 // main
-const renderMapIndexed = fn(t => props => {
+const renderMapIndexed = fn((t) => (props) => {
   const render = t.atOr(null, 'render', props)
   if (t.notType(render, 'Function')) {
     return null
   }
   const list = t.atOr(null, 'list', props)
   const items = t.isNil(list) ? t.atOr([], 'items', props) : list
+  const emptyRender = t.atOr(null, 'emptyRender', props)
+  if (t.and(t.noLen(items), t.isType(emptyRender, 'Function'))) {
+    return emptyRender({})
+  }
   return React.createElement(
     React.Fragment,
     {},
--- libs/ui-box-elements/src/When.jsx
+++ libs/ui-box-elements/src/When.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { fn } from '@z1/lib-ui-box'
 
 // main
-const renderWhen = fn(t => props => {
+const renderWhen = fn((t) => (props) => {
+  const nextProps = t.omit(['is', 'render', 'elseRender'], props)
   const is = t.atOr(false, 'is', props)
   if (t.not(is)) {
-    return null
+    const elseRender = t.atOr(null, 'elseRender', props)
+    if (t.isNil(elseRender)) {
+      return null
+    }
+    return elseRender(nextProps)
   }
   const render = t.atOr(null, 'render', props)
   if (t.isNil(render)) {
     return null
   }
-  return render(t.omit(['is', 'render'], props))
+  return render(nextProps)
 })
 
 export class When extends React.Component {

fcad037873e7b5bbbeecf7288f0be4eaa38c82d6
Barry Buck <jozi011@gmail.com>
2020-05-15 21:54:03 +0200

    export got on bucket node

--- kits/bucket-storage-node/src/index.js
+++ kits/bucket-storage-node/src/index.js
@@ -1,6 +1,8 @@
 import Fd from 'form-data'
 import { DownloaderHelper as Dl } from 'node-downloader-helper'
+import got from 'got'
 import { withRest as wr } from './main'
+// output
 export const withRest = wr
 export const DownloaderHelper = Dl
 export const FormData = Fd
@@ -8,5 +10,6 @@ const bsn = {
   withRest,
   FormData,
   Downloader: DownloaderHelper,
+  got,
 }
 export default bsn
